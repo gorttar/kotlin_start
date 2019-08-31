e74c1fc49d41e2d9c1d3eef09e167cd490d7c617
Andrey Antipov <andrey.antipov@maxifier.com>
2019-09-01 02:11:25 +0400

    add ps3 problem 3 sample

---
+++ src/main/kotlin/course/ps3/Problem3.kt
@@ -0,0 +1,15 @@
+package course.ps3
+
+/**
+ *
+ */
+
+object GetAvailableLettersSample {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        /**
+         * пример использования функции [getAvailableLetters]
+         */
+        println(getAvailableLetters(setOf('e', 'i', 'k', 'p', 'r', 's')))/*должно вывести abcdfghjlmnoqtuvwxyz*/
+    }
+}
