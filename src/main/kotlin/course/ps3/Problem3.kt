54256e39879801bfe6bc7755edd9247ceeceea7e
Andrey Antipov <andrey.antipov@maxifier.com>
2019-09-01 13:20:03 +0400

    add ps3 problem 3 tests

--- src/main/kotlin/course/ps3/Problem3.kt
+++ src/main/kotlin/course/ps3/Problem3.kt
@@ -1,8 +1,22 @@
 package course.ps3
 
+import lib.test.NamedCase.Companion.namedAs
+import lib.test.NamedCaseWithBody.Companion.passTo
+import lib.test.randomAlphabetPartition
+import lib.test.shouldBeEqualTo
+
 /**
+ * Третьей напишите функцию [getAvailableLetters]. Требования к функции описаны в её документации.
  *
+ * запусти, чтобы протестировать функцию [getAvailableLetters]
  */
+fun main() = (sequenceOf(
+        setOf('e', 'i', 'k', 'p', 'r', 's') to "abcdfghjlmnoqtuvwxyz"
+) + (1..10).asSequence().map {
+    randomAlphabetPartition().let { (xs, ys) -> ys.toSet() to xs }
+}).forEach { (arg, expected) ->
+    arg namedAs "Test for $arg" passTo { getAvailableLetters(arg) } shouldBeEqualTo expected
+}
 
 object GetAvailableLettersSample {
     @JvmStatic
