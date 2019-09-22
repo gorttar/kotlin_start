8019764d15e3cabf9485c4750b88ed23e8d78f85
Andrey Antipov <andrey.antipov@maxifier.com>
2019-09-22 22:02:48 +0400

    add list iteration example

--- src/main/kotlin/course/lecture5/iterables/Lists.kt
+++ src/main/kotlin/course/lecture5/iterables/Lists.kt
@@ -8,7 +8,7 @@ import lib.repr.repr
 fun main() {
     /**пустой список [Int]*/
     val list1: List<Int> = emptyList()
-    println("Пустой список: ${list1.repr.boldGreen}")
+    println("Пустой список:      ${list1.repr.boldGreen}")
 
     /**список [Int]*/
     val list2: List<Int> = listOf(1, 2, 5)
@@ -16,17 +16,30 @@ fun main() {
 
     /**список-то список, но вот чего?*/
     val list3 = listOf(2, 'a', 4, true)
-    println("Список чего-то: ${list3.repr.boldGreen}")
+    println("Список чего-то:     ${list3.repr.boldGreen}")
 
     val l = listOf(2, 1, 3)
     println("\nОперации со списком ${"l".magenta} = ${l.repr.boldGreen}:")
     withIndent {
-        println("Длина списка: ${"l.size".magenta} = ${l.size.repr.boldGreen}")
-        println("Содержится ли 2 в списке: ${"2 in l".magenta} = ${(2 in l).repr.boldGreen}")
-        println("Содержится ли 5 в списке: ${"5 in l".magenta} = ${(5 in l).repr.boldGreen}")
-        println("Элемент списка с индексом 0: ${"l[0]".magenta} или ${"l.get(0)".magenta} = ${l[0].repr.boldGreen}")
+        println("Длина списка:                                    ${"l.size".magenta} = ${l.size.repr.boldGreen}")
+        println("Содержится ли 2 в списке:                        ${"2 in l".magenta} = ${(2 in l).repr.boldGreen}")
+        println("Содержится ли 5 в списке:                        ${"5 in l".magenta} = ${(5 in l).repr.boldGreen}")
+        println("Элемент списка с индексом 0:          ${"l[0]".magenta} или ${"l.get(0)".magenta} = ${l[0].repr.boldGreen}")
         println("Элемент списка с индексом 2, увеличенный на 1: ${"l[2] + 1".magenta} = ${(l[2] + 1).repr.boldGreen}")
         val (i0, i1, i2) = l
-        println("Деконструкция списка: ${"(i0, i1, i2)".magenta} = ${"($i0, $i1, $i2)".boldGreen}")
+        println("Деконструкция списка:                      ${"(i0, i1, i2)".magenta} = ${"($i0, $i1, $i2)".boldGreen}")
+        println("\nПеребор (итерирование) элементов списка:")
+        withIndent {
+            var sumSquares = 0
+            for (x in l) {
+                sumSquares += x * x
+            }
+            println("Сумма квадратов, вычисленная с помощью цикла for: ${"sumSquares".magenta} = ${
+            sumSquares.repr.boldGreen
+            }")
+            println("Произведение кубов через map reduce:            ${"cubesProduct".magenta} = ${
+            l.asSequence().map { it * it * it }.reduce { acc, x -> acc * x }.repr.boldGreen
+            }")
+        }
     }
 }
