6b206ea91c220c6e4b376b35f8433091141cba37
Andrey Antipov <andrey.antipov@maxifier.com>
2019-09-29 23:22:36 +0400

    rename lNoTrace -> lnt

--- src/main/kotlin/course/lecture5/iterables/Lists.kt
+++ src/main/kotlin/course/lecture5/iterables/Lists.kt
@@ -19,9 +19,9 @@ fun main() {
     val list3 = listOf(2, 'a', 4, true)
     println("Список чего-то:     ${list3.repr.boldGreen}")
 
-    val lNoTrace = listOf(2, 1, 3)
-    val l = lNoTrace.trace("l")
-    println("\nОперации со списком ${"l".magenta} = ${lNoTrace.repr.boldGreen}:")
+    val lnt = listOf(2, 1, 3)
+    val l = lnt.trace("l")
+    println("\nОперации со списком ${"l".magenta} = ${lnt.repr.boldGreen}:")
     withIndent {
         println("Длина списка:                                    ${"l.size".magenta} = ${l.size.repr.boldGreen}")
         println("Содержится ли 2 в списке:                        ${"2 in l".magenta} = ${(2 in l).repr.boldGreen}")
--- src/main/kotlin/course/lecture5/iterables/MutableLists.kt
+++ src/main/kotlin/course/lecture5/iterables/MutableLists.kt
@@ -7,32 +7,32 @@ import lib.repr.repr
 import lib.trace.trace
 
 fun main() {
-    val lNoTrace = mutableListOf(2, 1, 3)
-    val l = lNoTrace.trace("l")
-    println("\nОперации с изменяемым списком ${"l".magenta} = ${lNoTrace.repr.boldGreen}:")
+    val lnt = mutableListOf(2, 1, 3)
+    val l = lnt.trace("l")
+    println("\nОперации с изменяемым списком ${"l".magenta} = ${lnt.repr.boldGreen}:")
     withIndent {
         l[1] = 5
         println("Изменение элемента с индексом 1 (${"l[1] = 5".magenta}):                          ${"l".magenta} = ${
-        lNoTrace.repr.boldGreen
+        lnt.repr.boldGreen
         }")
         l.add(5)
         println("Добавление элемента (${"l.add(5)".magenta}):                                      ${"l".magenta} = ${
-        lNoTrace.repr.boldGreen
+        lnt.repr.boldGreen
         }")
         l += 7
         println("Добавление элемента (${"l += 7".magenta}):                                        ${"l".magenta} = ${
-        lNoTrace.repr.boldGreen
+        lnt.repr.boldGreen
         }")
         l.addAll(listOf(8, 9, 10))
         println("Добавление элементов из другого списка (${"l.addAll(listOf(8, 9, 10))".magenta}): ${"l".magenta} = ${
-        lNoTrace.repr.boldGreen
+        lnt.repr.boldGreen
         }")
         l += listOf(11, 12, 13)
         println("Добавление элементов из другого списка (${"l += listOf(11, 12, 13)".magenta}):    ${"l".magenta} = ${
-        lNoTrace.repr.boldGreen
+        lnt.repr.boldGreen
         }")
         println("Удаление элемента с индексом 10 (элемент = ${l.removeAt(10).repr.magenta}):                      ${"l".magenta} = ${
-        lNoTrace.repr.boldGreen
+        lnt.repr.boldGreen
         }")
     }
 }
