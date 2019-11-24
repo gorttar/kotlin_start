b4237b56398c51c9a534e70f0830c6c146846cd1
Andrey Antipov <andrey.antipov@maxifier.com>
2019-11-25 02:39:36 +0400

    add recordIO function

--- src/main/kotlin/lib/test/io/IORecorder.kt
+++ src/main/kotlin/lib/test/io/IORecorder.kt
@@ -1,7 +1,17 @@
 package lib.test.io
 
-enum class OperationType { IN, OUT }
+import lib.control.ManagedValue
+import lib.control.on
+import lib.test.io.OperationType.ERR
+import lib.test.io.OperationType.IN
+import lib.test.io.OperationType.OUT
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+
+enum class OperationType { IN, OUT, ERR }
 class IOOperationChunk(val operationType: OperationType, content: ByteArray) {
+    constructor(operationType: OperationType, content: String) : this(operationType, content.toByteArray())
     val content: ByteArray = content.copyOf()
         get() = field.copyOf()
 
@@ -21,7 +31,7 @@ class IORecorder {
                 else emptyList()
 
     fun recordByte(byte: Byte, operationType: OperationType) = recordBytes(byteArrayOf(byte), operationType)
-    fun recordBytes(bytes: ByteArray, operationType: OperationType, off: Int = 0, len: Int = bytes.size) {
+    fun recordBytes(bytes: ByteArray, operationType: OperationType, off: Int = 0, len: Int = bytes.size - off) {
         if (::curOperationType.isInitialized && curOperationType != operationType) {
             _operationChunks += IOOperationChunk(curOperationType, curByteChunk)
             curByteChunk = byteArrayOf()
@@ -29,4 +39,34 @@ class IORecorder {
         curOperationType = operationType
         curByteChunk += bytes.copyOfRange(off, off + len)
     }
+}
+
+class RecordedInStream(buf: ByteArray, private val ioRecorder: IORecorder) : ByteArrayInputStream(buf) {
+    override fun read() = super.read().also { ioRecorder.recordByte(it.toByte(), IN) }
+    override fun read(b: ByteArray, off: Int, len: Int) =
+            super.read(b, off, len).also { ioRecorder.recordBytes(bytes = b, operationType = IN, off = off, len = it) }
+}
+
+sealed class RecordedOutputStream(
+        private val ioRecorder: IORecorder,
+        private val operationType: OperationType
+) : ByteArrayOutputStream() {
+    override fun write(b: Int) = super.write(b).also { ioRecorder.recordByte(b.toByte(), operationType) }
+    override fun write(b: ByteArray, off: Int, len: Int) = super.write(b, off, len).also {
+        ioRecorder.recordBytes(bytes = b, operationType = operationType, off = off, len = len)
+    }
+}
+
+class RecordedOutStream(ioRecorder: IORecorder) : RecordedOutputStream(ioRecorder, OUT)
+class RecordedErrStream(ioRecorder: IORecorder) : RecordedOutputStream(ioRecorder, ERR)
+
+fun recordIO(input: String, block: () -> Unit): List<IOOperationChunk> = IORecorder().let { recorder ->
+    ManagedValue(System::`in`, System::setIn).on(RecordedInStream(input.toByteArray(), recorder)) {
+        ManagedValue(System::out, System::setOut).on(PrintStream(RecordedOutStream(recorder))) {
+            ManagedValue(System::err, System::setErr).on(PrintStream(RecordedErrStream(recorder))) {
+                block()
+            }
+        }
+    }
+    recorder.operationChunks
 }
--- src/test/kotlin/lib/test/io/IORecorderTest.kt
+++ src/test/kotlin/lib/test/io/IORecorderTest.kt
@@ -1,8 +1,9 @@
 package lib.test.io
 
 import assertk.assertThat
+import assertk.assertions.containsExactly
 import assertk.assertions.isEmpty
-import assertk.assertions.isEqualTo
+import lib.test.io.OperationType.ERR
 import lib.test.io.OperationType.IN
 import lib.test.io.OperationType.OUT
 import org.testng.annotations.Test
@@ -14,70 +15,97 @@ class IORecorderTest {
     @Test
     fun recordByte() = IORecorder().run {
         recordByte('a'.toByte(), IN)
-        assertThat(operationChunks).isEqualTo(listOf(IOOperationChunk(IN, "a".toByteArray())))
+        assertThat(operationChunks).containsExactly(IOOperationChunk(IN, "a"))
 
         recordByte('b'.toByte(), IN)
-        assertThat(operationChunks).isEqualTo(listOf(IOOperationChunk(IN, "ab".toByteArray())))
+        assertThat(operationChunks).containsExactly(IOOperationChunk(IN, "ab"))
 
         recordByte('c'.toByte(), OUT)
-        assertThat(operationChunks)
-                .isEqualTo(
-                        listOf(
-                                IOOperationChunk(IN, "ab".toByteArray()),
-                                IOOperationChunk(OUT, "c".toByteArray())
-                        )
-                )
-        recordByte('d'.toByte(), OUT)
-        assertThat(operationChunks)
-                .isEqualTo(
-                        listOf(
-                                IOOperationChunk(IN, "ab".toByteArray()),
-                                IOOperationChunk(OUT, "cd".toByteArray())
-                        )
-                )
+        assertThat(operationChunks).containsExactly(
+                IOOperationChunk(IN, "ab"),
+                IOOperationChunk(OUT, "c")
+        )
+
+        recordByte('d'.toByte(), ERR)
+        assertThat(operationChunks).containsExactly(
+                IOOperationChunk(IN, "ab"),
+                IOOperationChunk(OUT, "c"),
+                IOOperationChunk(ERR, "d")
+        )
     }
 
     @Test
     fun `recordBytes empty array`() = IORecorder().run {
         recordBytes(byteArrayOf(), IN)
-        assertThat(operationChunks).isEqualTo(listOf(IOOperationChunk(IN, byteArrayOf())))
+        assertThat(operationChunks).containsExactly(IOOperationChunk(IN, ""))
 
         recordBytes(byteArrayOf(), IN)
-        assertThat(operationChunks).isEqualTo(listOf(IOOperationChunk(IN, byteArrayOf())))
+        assertThat(operationChunks).containsExactly(IOOperationChunk(IN, ""))
 
         recordBytes(byteArrayOf(), OUT)
-        assertThat(operationChunks)
-                .isEqualTo(listOf(IOOperationChunk(IN, byteArrayOf()), IOOperationChunk(OUT, byteArrayOf())))
+        assertThat(operationChunks).containsExactly(
+                IOOperationChunk(IN, ""),
+                IOOperationChunk(OUT, "")
+        )
 
-        recordBytes(byteArrayOf(), OUT)
-        assertThat(operationChunks)
-                .isEqualTo(listOf(IOOperationChunk(IN, byteArrayOf()), IOOperationChunk(OUT, byteArrayOf())))
+        recordBytes(byteArrayOf(), ERR)
+        assertThat(operationChunks).containsExactly(
+                IOOperationChunk(IN, ""),
+                IOOperationChunk(OUT, ""),
+                IOOperationChunk(ERR, "")
+        )
     }
 
     @Test
     fun `recordBytes non empty array`() = IORecorder().run {
         recordBytes("ab".toByteArray(), IN)
-        assertThat(operationChunks).isEqualTo(listOf(IOOperationChunk(IN, "ab".toByteArray())))
+        assertThat(operationChunks).containsExactly(IOOperationChunk(IN, "ab"))
 
         recordBytes("cd".toByteArray(), IN)
-        assertThat(operationChunks).isEqualTo(listOf(IOOperationChunk(IN, "abcd".toByteArray())))
+        assertThat(operationChunks).containsExactly(IOOperationChunk(IN, "abcd"))
 
         recordBytes("ef".toByteArray(), OUT)
-        assertThat(operationChunks)
-                .isEqualTo(
-                        listOf(
-                                IOOperationChunk(IN, "abcd".toByteArray()),
-                                IOOperationChunk(OUT, "ef".toByteArray())
-                        )
-                )
+        assertThat(operationChunks).containsExactly(
+                IOOperationChunk(IN, "abcd"),
+                IOOperationChunk(OUT, "ef")
+        )
 
-        recordBytes("gh".toByteArray(), OUT)
-        assertThat(operationChunks)
-                .isEqualTo(
-                        listOf(
-                                IOOperationChunk(IN, "abcd".toByteArray()),
-                                IOOperationChunk(OUT, "efgh".toByteArray())
-                        )
-                )
+        recordBytes("gh".toByteArray(), ERR)
+        assertThat(operationChunks).containsExactly(
+                IOOperationChunk(IN, "abcd"),
+                IOOperationChunk(OUT, "ef"),
+                IOOperationChunk(ERR, "gh")
+        )
+    }
+
+    @Test
+    fun `recordBytes off len`() = IORecorder().run {
+        recordBytes(bytes = "abcd".toByteArray(), operationType = IN, off = 1, len = 2)
+        assertThat(operationChunks).containsExactly(IOOperationChunk(IN, "bc"))
+    }
+}
+
+class RecordIOTest {
+    @Test
+    fun recordIOTest() {
+        val actual = recordIO("hello\n") {
+            println("Start")
+            val readLine = readLine()
+            println("Read: $readLine")
+            System.err.println("ErrRead: $readLine")
+            println("End")
+            System.err.print("ErrEnd")
+            System.err.println()
+        }
+        val cr = if (actual.first().content.dropLast(1).last() == '\r'.toByte()) "\r\n"
+        else "\n"
+        assertThat(actual).containsExactly(
+                IOOperationChunk(OUT, "Start$cr"),
+                IOOperationChunk(IN, "hello\n"),
+                IOOperationChunk(OUT, "Read: hello$cr"),
+                IOOperationChunk(ERR, "ErrRead: hello$cr"),
+                IOOperationChunk(OUT, "End$cr"),
+                IOOperationChunk(ERR, "ErrEnd$cr")
+        )
     }
 }
