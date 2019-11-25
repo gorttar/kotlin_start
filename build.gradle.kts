cf101df16dcffd6a57238895db4a3163ae158d3e
Andrey Antipov <andrey.antipov@maxifier.com>
2019-11-26 03:20:25 +0400

    add IOTest

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,11 +20,15 @@ dependencies {
     implementation(group = kotlinGroup, name = "kotlin-stdlib-jdk8", version = kotlinVersion)
     implementation(group = kotlinGroup, name = "kotlin-reflect", version = kotlinVersion)
     implementation(group = kotlinGroup, name = "kotlin-script-runtime", version = kotlinVersion)
+    implementation(group = kotlinGroup, name = "kotlin-test", version = kotlinVersion)
     implementation(group = "org.jetbrains.kotlinx", name = "kotlinx-coroutines-core", version = "1.3.5")
     implementation(group = "com.github.ajalt", name = "mordant", version = "1.2.1")
     implementation(group = "com.nhaarman", name = "mockito-kotlin", version = "1.6.0")
-    implementation(group = "com.opencsv", name = "opencsv", version =  "5.0")
+    implementation(group = "com.opencsv", name = "opencsv", version = "5.0")
+    implementation(group = "com.willowtreeapps.assertk", name = "assertk-jvm", version = "0.20")
+
     implementation(project(":visibility"))
+
     testImplementation(group = "org.testng", name = "testng", version = "6.14.3")
     testImplementation(group = "com.willowtreeapps.assertk", name = "assertk-jvm", version = "0.20")
 }
---
+++ src/test/kotlin/lib/test/io/IOTestTest.kt
@@ -0,0 +1,23 @@
+package lib.test.io
+
+import lib.test.io.IOScenario.Companion.runIOTest
+
+fun main() {
+    runIOTest({
+        expectLine("Start")
+        sendLine("errHello")
+        expectErrLine("ErrRead: errHello")
+        sendLine("hello")
+        expectLine(
+                """Read: hello
+                |End
+                """.trimMargin()
+        )
+    }) {
+        println("Start")
+        System.err.println("ErrRead: ${readLine()}")
+        println("Read: ${readLine()}")
+        println("End")
+    }
+}
+
