41f37d6e3f23186bee3a377426090f8eff7ee483
Andrey Antipov <gorttar@gmail.com>
2019-12-08 04:33:20 +0400

    add explicit types for public declarations

--- src/main/kotlin/lib/graphics/turtle/awt/GraphicsTurtleCore.kt
+++ src/main/kotlin/lib/graphics/turtle/awt/GraphicsTurtleCore.kt
@@ -39,7 +39,7 @@ class GraphicsTurtleCore(
     override fun pen(width: Float): Unit =
         graphics { stroke = BasicStroke(width, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND) }
 
-    override fun showTurtle() = stateView.let {
+    override fun showTurtle(): Unit = stateView.let {
         if (it.isVisible) graphics {
             val oldStroke = stroke
             stroke = BasicStroke(2f)
@@ -73,6 +73,6 @@ class GraphicsTurtleCore(
 
 typealias Pos = Pair<Double, Double>
 
-val Pos.x get() = first
-val Pos.y get() = second
+val Pos.x: Double get() = first
+val Pos.y: Double get() = second
 operator fun Pos.plus(o: Pos): Pos = x + o.x to y + o.y
